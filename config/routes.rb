f438e5c74a315a0aa639d71a9fc597fb9b4d785d
Anisoara <barbalatana@gmail.com>
2018-08-02 23:46:34 +0200

    last commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get    '/signup',  to: 'users#new'
+  get    '/signup', to: 'users#new'
   post   '/signup', to: 'users#create'
 
   get    '/login',   to: 'sessions#new'
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   delete '/logout',  to: 'sessions#destroy'
   get    '/univers', to: 'static_pages#univers'
+  get    '/profile', to: 'users#show'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
